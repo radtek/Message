f6785c51827e0b2b34ca9d07f8f36234562d7554
yuan272958293 <272958293@qq.com>
2018-03-14 20:19:41 +0800

    备份

--- WindowsFormsApplication1/SendMsg.cs
+++ WindowsFormsApplication1/SendMsg.cs
@@ -45,8 +45,8 @@ namespace WindowsFormsApplication1
                                 
                                 foreach (DataRow dr2 in dt2.Rows)
                                 {
-                                    phone = dr2["EmpMobileNum"].ToString();
-                                    //phone = "15261277153";
+                                    //phone = dr2["EmpMobileNum"].ToString();
+                                    phone = "15261277153";
                                     doctor = dr2["EMPNAME"].ToString();
                                     //string sms = "5272718510496|" + doctor + "|" + patient + "|" + "在" + Item + "项目上分值为" + score;
                                     //string strSql = "insert into sms_outbox (sismsid, extcode, destaddr, messagecontent, reqdeliveryreport,msgfmt, sendmethod, requesttime, applicationid)VALUES('" + id + "', '','" + phone + "', '" + sms + "',1,15,2, '" + _add_time + "', 'APP128')";
@@ -58,76 +58,103 @@ namespace WindowsFormsApplication1
                                     string _add_time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
                                     if (!string.IsNullOrEmpty(patient) && !string.IsNullOrEmpty(Item) && !string.IsNullOrEmpty(score) && !string.IsNullOrEmpty(phone) && !string.IsNullOrEmpty(doctor))
                                     {
-                                        //if (!bll.Exists("01", phone))
-                                        
-                                        DateTime beginTime = Convert.ToDateTime(bll.getSentSms());
-                                        DateTime endTime = beginTime.AddMinutes(5);
-                                        bool s = bll.ExistMinute(phone, beginTime.ToString("yyyy-MM-dd HH:mm:ss"), endTime.ToString("yyyy-MM-dd HH:mm:ss"));
-                                        bool hasValue = bll2.Exists(phone, Item, score, patient_id, 0);
-                                        if (s == true)//五分钟之内
+                                        if (!bll.Exists("01", phone))
                                         {
-                                            if (hasValue)//存在
+                                            Model.Sms_outbox model = new Model.Sms_outbox();
+                                            model.sismsid = id;
+                                            model.extcode = "01";
+                                            model.destaddr = phone;
+                                            model.messagecontent = messageID + "|" + doctor + "|" + patient + "|" + Item + "|" + score + "|" + limit;
+                                            model.reqdeliveryreport = 1;
+                                            model.msgfmt = 15;
+                                            model.sendmethod = 2;
+                                            model.requesttime = _add_time;
+                                            model.applicationid = "APP128";
+                                            if (bll.Add(model))
                                             {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                                                Model.BIF01022 model = new Model.BIF01022();
-                                                model.Patient_id = patient_id;
-                                                model.Item_name = Item;
-                                                model.Current_result = score;
-                                                model.EmpMobileNum = phone;
-                                                model.State = 1;
-                                                bll2.Update(model);
-=======
-                                                 ++i;
->>>>>>> 66ccdcc4874453ee0178be9d2f23e6d3a62b10a5
-=======
-                                                 ++i;
->>>>>>> 66ccdcc4874453ee0178be9d2f23e6d3a62b10a5
-=======
-                                                 ++i;
->>>>>>> 66ccdcc4874453ee0178be9d2f23e6d3a62b10a5
-=======
-                                                 ++i;
->>>>>>> 66ccdcc4874453ee0178be9d2f23e6d3a62b10a5
+                                                return i++;
                                             }
-                                            
-                                        }
-                                        else
-                                        {
-                                            
-                                            if (!hasValue || (hasValue&&!bll2.Exists(phone, Item, score, patient_id, 1)))//不存在或者状态为0 发送
-                                            {
-                                                Model.Sms_outbox model = new Model.Sms_outbox();
-                                                //model.sismsid = id;
-                                                //model.extcode = "01";
-                                                //model.destaddr = phone;
-                                                //model.messagecontent = messageID + "|" + doctor + "|" + patient + "|" + Item + "|" + score + "|" + limit;
-                                                //model.reqdeliveryreport = 1;
-                                                //model.msgfmt = 15;
-                                                //model.sendmethod = 2;
-                                                //model.requesttime = _add_time;
-                                                //model.applicationid = "APP128";
-                                                //if (bll.Add(model))
-                                                //{
-                                                //    Model.BIF01022 model2 = new Model.BIF01022();
-                                                //    model2.Patient_id = patient_id;
-                                                //    model2.Patient_name = patient;
-                                                //    model2.Item_name = Item;
-                                                //    model2.Current_result = score;
-                                                //    model2.EmpMobileNum = phone;
-                                                //    model2.EMPNAME = doctor;
-                                                //    model2.State = 0;
-                                                //    model2.Add_time = _add_time;
-                                                //    bll2.Add(model2);
-                                                //    return ++i;
-                                                //}
-                                            }
-                                            
-                                        }
-                                            
 
+                                        }
+
+                                        //DateTime beginTime = Convert.ToDateTime(bll.getSentSms());
+                                        //DateTime endTime = beginTime.AddMinutes(5);
+                                        //bool s1 = bll.ExistMinute(phone, beginTime.ToString("yyyy-MM-dd HH:mm:ss"), endTime.ToString("yyyy-MM-dd HH:mm:ss"));
+                                        //bool hasValue = bll2.Exists(phone, Item, score, patient_id, 0);
+                                        //if (s1 == true)//五分钟之内
+                                        //{
+                                        //    if (hasValue)//存在
+                                        //    {
+                                        //        Model.BIF01022 model = new Model.BIF01022();
+                                        //        model.Patient_id = patient_id;
+                                        //        model.Item_name = Item;
+                                        //        model.Current_result = score;
+                                        //        model.EmpMobileNum = phone;
+                                        //        model.State = 1;
+                                        //        bll2.Update(model);
+                                        //    }
+                                        //    else
+                                        //    {
+                                        //        Model.Sms_outbox model = new Model.Sms_outbox();
+                                        //        model.sismsid = id;
+                                        //        model.extcode = "01";
+                                        //        model.destaddr = phone;
+                                        //        model.messagecontent = messageID + "|" + doctor + "|" + patient + "|" + Item + "|" + score + "|" + limit;
+                                        //        model.reqdeliveryreport = 1;
+                                        //        model.msgfmt = 15;
+                                        //        model.sendmethod = 2;
+                                        //        model.requesttime = _add_time;
+                                        //        model.applicationid = "APP128";
+                                        //        if (bll.Add(model))
+                                        //        {
+                                        //            Model.BIF01022 model2 = new Model.BIF01022();
+                                        //            model2.Patient_id = patient_id;
+                                        //            model2.Patient_name = patient;
+                                        //            model2.Item_name = Item;
+                                        //            model2.Current_result = score;
+                                        //            model2.EmpMobileNum = phone;
+                                        //            model2.EMPNAME = doctor;
+                                        //            model2.State = 0;
+                                        //            model2.Add_time = _add_time;
+                                        //            bll2.Add(model2);
+                                        //            return ++i;
+                                        //        }
+
+                                        //    }
+                                        //}
+                                        //else
+                                        //{
+
+                                        //    if (!bll2.Exists(phone, Item, score, patient_id, 1))//不存在或者状态为0 发送
+                                        //    {
+                                        //        Model.Sms_outbox model = new Model.Sms_outbox();
+                                        //        model.sismsid = id;
+                                        //        model.extcode = "01";
+                                        //        model.destaddr = phone;
+                                        //        model.messagecontent = messageID + "|" + doctor + "|" + patient + "|" + Item + "|" + score + "|" + limit;
+                                        //        model.reqdeliveryreport = 1;
+                                        //        model.msgfmt = 15;
+                                        //        model.sendmethod = 2;
+                                        //        model.requesttime = _add_time;
+                                        //        model.applicationid = "APP128";
+                                        //        if (bll.Add(model))
+                                        //        {
+                                        //            Model.BIF01022 model2 = new Model.BIF01022();
+                                        //            model2.Patient_id = patient_id;
+                                        //            model2.Patient_name = patient;
+                                        //            model2.Item_name = Item;
+                                        //            model2.Current_result = score;
+                                        //            model2.EmpMobileNum = phone;
+                                        //            model2.EMPNAME = doctor;
+                                        //            model2.State = 0;
+                                        //            model2.Add_time = _add_time;
+                                        //            bll2.Add(model2);
+                                        //            return ++i;
+                                        //        }
+
+                                        //    }
+
+                                        //}
                                         //string strSql = string.Format("insert into sms_outbox (sismsid, extcode, destaddr, messagecontent, reqdeliveryreport,msgfmt, sendmethod, requesttime, applicationid)VALUES('{0}', '','{1}', '5272718510513|{2}|{3}|{4}|{5}|{6}',1,15,2, '{7}', 'APP128')", id, phone, doctor, patient, Item, score, limit, _add_time);
                                         //i=DbHelperMySQL.ExecuteSql(strSql);
                                         //return i;
